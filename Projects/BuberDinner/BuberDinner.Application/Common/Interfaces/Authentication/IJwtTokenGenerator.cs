5be39e10a416b19d8daafabca075f43ad0586051
Yasse7 <yasserjerrari2001@gmail.com>
2024-03-11 11:03:01 +0100

    jwt

--- Projects/BuberDinner/BuberDinner.Application/Common/Interfaces/Authentication/IJwtTokenGenerator.cs
+++
@@ -1,6 +0,0 @@
-namespace BuberDinner.Common.interfaces.Authentication;
-
-public interface IJwtTokenGenerator
- {
-    string GenerateToken(Guid UserId,string firstname,string lastname);
-}
--- Projects/BuberDinner/BuberDinner.Application/Services/Authentification/IAuthentificationService.cs
+++
@@ -1,6 +0,0 @@
-namespace BuberDinner.Application.Services.Authentication;
-
-public interface IAuthentificationService{
-    AuthenticationResult Register(string fname,string lname ,string email,string password ) ;
-    AuthenticationResult Login(string email,string password ) ;
-}
